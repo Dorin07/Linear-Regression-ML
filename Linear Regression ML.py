01c87dd70b73862ea727db2deed99aaf123931af
Dorin07 <47871786+Dorin07@users.noreply.github.com>
2021-02-19 22:52:53 +0200

    Update.

--- Linear Regression ML.py
+++ Linear Regression ML.py
@@ -4,4 +4,4 @@ import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
-data = pd.read_csv("student-mat.csv", sep = ";") 
+data = pd.read_csv("student-mat.csv", sep=";")
