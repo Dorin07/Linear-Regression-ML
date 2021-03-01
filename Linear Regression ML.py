a38e41ea30a52fd680d2266d2f74ee910ed0d137
Dorin07 <47871786+Dorin07@users.noreply.github.com>
2021-03-02 00:38:12 +0200

    Implement the linear regression algorithm.

--- Linear Regression ML.py
+++ Linear Regression ML.py
@@ -4,6 +4,10 @@ import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
+"""
+    Predict after grades 1,2 and 3, study time, failures and finally absences.
+"""
+
 data = pd.read_csv("student-mat.csv", sep=";")
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
@@ -12,4 +16,23 @@ predict = "G3"
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+x_train, x_test,  y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+
+"""
+    Calculate the accuracy of the data training model.
+"""
+acc = linear.score(x_test, y_test)
+
+print(acc)
+
+print("Coefficient: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
